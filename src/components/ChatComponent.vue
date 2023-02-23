1b2d1ab417309799b729f93ace58a089c4d4f94e
Hassan <hassanila97@users.noreply.github.com>
2023-02-23 15:25:54 +0100

    Update ChatComponent.vue
    add deleteChat()
    add user-select: none to prevent unwanted text selection when clicking on chat
    add startNewChat section
    css improvements

--- src/components/ChatComponent.vue
+++ src/components/ChatComponent.vue
@@ -3,17 +3,26 @@ import users from "../assets/data/users.json";
 import moment from "moment";
 </script>
 
-// TODO: set globally box-sizing property to border-box
+// TODO: set globally box-sizing property to border-box and margin padding to 0
 
 <template>
   <div id="chat">
     <div id="chatSelect">
+      <div id="newChatSelect">
+        <input v-if="!showNewChatSection" type="button" value="Start new chat" @click="showNewChatSection = true">
+        <div v-else>
+          <input type="button" value="Close" @click="showNewChatSection = false">
+          <div class="newChatSelectUser" v-for="user in users">
+            {{ user.userName }}
+          </div>
+        </div>
+      </div>
       <div
         :key="index"
         v-for="(chat, index) in loggedInUser.chats"
         @click="openChat(chat.userName)"
-        :class="chat.userName === currentChatUsername ? 'selected' : ''"
-      ><img class="avatar" :src="getUser(chat.userName).avatar" alt="avatar" />
+        :class="chat.userName === activeChatUser?.userName ? 'selected' : ''"
+      ><img class="avatar" :src="getUser(chat.userName).avatar" alt="avatar" @click.stop="goToProfile(chat.userName)"/>
         <div>
 
         <p class="name">{{ getUser(chat.userName).firstName }} {{ getUser(chat.userName).lastName }}</p>
@@ -22,16 +31,20 @@ import moment from "moment";
           {{ chat.messages[chat.messages.length - 1].type }}: {{ chat.messages[chat.messages.length - 1].message }}
         </p>
         </div>
+        <div id="chatSelectOptions">
+          <input type="button" value="X" @click.stop="deleteChat(chat.userName)">
+        </div>
+
       </div>
       <div v-if="!loggedInUser.chats.length">
         <p>You don't have any open chats...</p>
       </div>
     </div>
-    <div id="selectedChatContainer" v-if="currentChatUsername !== null">
+    <div id="selectedChatContainer" v-if="activeChatUser !== null">
       <div
         v-for="(message, index) in loggedInUser.chats.find(
-          (chat) => chat.userName === currentChatUsername
-        ).messages"
+          (chat) => chat.userName === activeChatUser?.userName
+        )?.messages"
         :class="'chatMessage ' + message.type"
         :key="index"
       >
@@ -39,9 +52,9 @@ import moment from "moment";
         {{ message.message }}
       </div>
       <div id="chatInputsContainer">
-        <form @submit="(e) => e.preventDefault()">
+        <form @submit="sendMessage">
           <input type="text" v-model="chatMessageInput" />
-          <input type="submit" @click="sendMessage()" value="Send" />
+          <input type="submit" value="Send" />
         </form>
 
       </div>
@@ -54,11 +67,12 @@ export default {
   name: "ChatComponent",
   data() {
     return {
-      currentChatUsername: null,
+      activeChatUser: null,
       loading: true,
       // TODO: replace this with actual logged in user
       loggedInUser: users[0],
       chatMessageInput: "",
+      showNewChatSection: false
     };
   },
   methods: {
@@ -69,59 +83,76 @@ export default {
       // TODO: ability to open chat with new user
 
       // if chat is already open then close it
-      if (this.currentChatUsername === userName) {
-        this.currentChatUsername = null;
+      if (this.activeChatUser?.userName === userName) {
+        this.activeChatUser = null;
       } else {
-        this.currentChatUsername = userName;
+        this.activeChatUser = getUser(userName);
       }
     },
-    sendMessage() {
-      if (!this.chatMessageInput) {
-        return;
-      }
+    deleteChat(userName) {
+      this.chatMessageInput = "";
+
+      console.log('Removing chat with ' + userName)
 
       // add message to current logged in user messages list
-      this.loggedInUser.chats
-        .find((chat) => chat.userName === this.currentChatUsername)
-        .messages.push({
+      this.loggedInUser.chats.splice(
+        this.loggedInUser.chats.findIndex((chat) => chat.userName === userName),
+        1
+      );
+    },
+    sendMessage(evt) {
+      evt.preventDefault();
+
+      if (this.chatMessageInput) {
+
+        // add message to current logged in user messages list
+        this.loggedInUser.chats
+            .find((chat) => chat.userName === this.activeChatUser?.userName)
+            .messages.push({
           time: moment().unix(),
           message: this.chatMessageInput,
           type: "sent",
         });
 
-      // also add message to the receiving user message list
-      const receivingUser = users.find(
-        (user) => user.userName === this.currentChatUsername
-      );
-      let receivingUserChat = receivingUser.chats.find(
-        (chat) => chat.userName == this.loggedInUser.userName
-      );
-
-      if (receivingUserChat) {
-        console.log("Chat exists in receiving users chat list");
-        receivingUserChat.messages.push({
-          time: moment().unix(),
-          message: this.chatMessageInput,
-          type: "received",
-        });
-      } else {
-        console.log(
-          "Chat doesn't exists in receiving users chat list, creating it now"
+        // also add message to the receiving user message list
+        const receivingUser = users.find(
+            (user) => user.userName === this.activeChatUser?.userName
         );
-        receivingUser.chats.push({
-          userName: this.loggedInUser.userName,
-          messages: [
-            {
-              time: moment().unix(),
-              message: this.chatMessageInput,
-              type: "received",
-            },
-          ],
-        });
+        let receivingUserChat = receivingUser.chats.find(
+            (chat) => chat.userName == this.loggedInUser.userName
+        );
+
+        if (receivingUserChat) {
+          console.log("Chat exists in receiving users chat list");
+          receivingUserChat.messages.push({
+            time: moment().unix(),
+            message: this.chatMessageInput,
+            type: "received",
+          });
+        } else {
+          console.log(
+              "Chat doesn't exists in receiving users chat list, creating it now"
+          );
+          receivingUser.chats.push({
+            userName: this.loggedInUser.userName,
+            messages: [
+              {
+                time: moment().unix(),
+                message: this.chatMessageInput,
+                type: "received",
+              },
+            ],
+          });
+        }
+
+        this.chatMessageInput = "";
       }
 
-      this.chatMessageInput = "";
     },
+    goToProfile(userName) {
+      // TODO: redirect to user profile page
+      console.log(getUser(userName))
+    }
   },
 };
 
@@ -168,18 +199,27 @@ function getUser(userName) {
 #chatSelect {
   display: flex;
   flex-direction: column;
-  gap: 1rem;
   min-width: fit-content;
   border: 0.1rem solid red;
   border-radius: 10px;
   padding: 0.5rem;
+
+  -webkit-touch-callout: none; /* iOS Safari */
+  -webkit-user-select: none; /* Safari */
+  -khtml-user-select: none; /* Konqueror HTML */
+  -moz-user-select: none; /* Old versions of Firefox */
+  -ms-user-select: none; /* Internet Explorer/Edge */
+  user-select: none; /* Non-prefixed version, currently
+                                  supported by Chrome, Edge, Opera and Firefox */
 }
 
 #chatSelect div {
-display: flex;
-  gap: .5rem;
+  display: flex;
+  gap: 0 1rem;
   border-radius: 10px;
-padding: .5rem 1rem;
+  justify-content: space-between;
+  align-items: center;  cursor: pointer;
+  padding: 1rem 0.5rem;
 }
 
 #chatSelect > div > div {
@@ -187,7 +227,7 @@ padding: .5rem 1rem;
   justify-content: space-around;
   flex-direction: column;
   align-content: center;
-  cursor: pointer;
+
 }
 
 .name {
@@ -224,10 +264,15 @@ padding: .5rem 1rem;
   border-radius: 50%;
   height: 4rem;
   border: 0.2rem solid green;
+  cursor: pointer;
 }
 
 #chatInputsContainer {
   margin: auto 0 0 auto;
 
+}
+
+.newChatSelectUser {
+  border: .1rem solid red;
 }
 </style>
