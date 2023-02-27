35a525dac8330f81525765c55ac62d876cd71780
Anja505505 <112382801+Anja505505@users.noreply.github.com>
2023-02-27 16:26:46 +0100

    sign up form

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,10 @@
-<script setup></script>
+<script setup>
+import signUp from "../components/signUp.vue";
+</script>
 
 <template>
   <main>
     <h2>Hitta din kärlek</h2>
   </main>
+  <signUp />
 </template>
