bc20eccc53b62d314cd7c85bfcbc3239dc9d7298
Ira86 <kudlai.irina@outlook.com>
2023-02-27 13:30:27 +0100

    Installed bootstrap, changes in Header and created Footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,42 @@
+<script>
+    import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+</script>
+
+<template>
+
+
+  <!-- Footer -->
+  <footer class="text-center text-lg-start text-dark" style="background: linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, #252525 100%);"
+          >
+    <!-- Section: Social media -->
+    <section
+             class="d-flex justify-content-between p-4 text-white"
+             >
+      <!-- Left -->
+      <div class="me-5">
+        <span>Terms & Conditions</span>
+      </div>
+      <!-- Left -->
+        <div class="text-center">
+        Copyright © 2023 | ITHS Team 7
+        </div>
+      <!-- Right -->
+      <div>
+        <a href="" class="text-white me-2">
+         <img src="../assets/images/linkedin_white.svg" width="24" height="24"/>   
+        </a>
+        <a href="" class="text-white me-2">
+            <img src="../assets/images/facebook_white.svg" width="24" height="24"/>       
+        </a>
+        <a href="" class="text-white me-2">
+          <img src="../assets/images/instagram_white.svg" width="24" height="24"/> 
+        </a>
+      </div>
+      <!-- Right -->
+    </section>
+    <!-- Section: Social media -->
+
+  </footer>
+
+
+</template>
