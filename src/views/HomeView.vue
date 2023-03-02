dcb61996afe8c5b56d1dc3010422d4c643d73f75
Hassan <hassanila97@users.noreply.github.com>
2023-03-02 16:35:19 +0100

    Revert "add HomeView.vue bg-image"

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,12 +2,6 @@
 import HeroSec from "../components/HeroSection.vue";
 </script>
 
-<style>
-body {
-  background-image: url("src/assets/dating.jpg");
-}
-</style>
-
 <template>
   <HeroSec></HeroSec>
 </template>
