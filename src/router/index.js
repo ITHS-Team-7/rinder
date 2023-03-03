4217f4882163f6e4325881b019363651e06a6ae7
katzlerjulia <julia.katzler@gmail.com>
2023-03-03 10:55:37 +0100

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import ChatView from "../views/ChatView.vue";
-import ContactView from "../views/ContactView.vue";
+//import TermsPage from "../views/TermsPage.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
