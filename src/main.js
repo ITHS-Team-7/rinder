d7193b3e26269d4daaffe3c491c9af0c9499eb4c
Maximon1997 <max@mellanslaget.se>
2023-03-03 11:56:36 +0100

    chat cards css

--- src/main.js
+++ src/main.js
@@ -3,8 +3,6 @@ import "bootstrap/dist/css/bootstrap.css";
 import bootstrap from "bootstrap/dist/js/bootstrap.bundle.js";
 import App from "./App.vue";
 import router from "./router";
-import "bootstrap/dist/css/bootstrap.css";
-import bootstrap from "bootstrap/dist/js/bootstrap.bundle.js";
 import "./assets/main.css";
 
 /* import the fontawesome core */
