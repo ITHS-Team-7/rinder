1e3a4eeb7bde615970eeb943dee2b376a99725bf
Anja505505 <112382801+Anja505505@users.noreply.github.com>
2023-03-06 13:41:11 +0100

    resolved more conflicts

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,9 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import ChatView from "../views/ChatView.vue";
-<<<<<<< HEAD
 import AccountView from "../views/AccountView.vue";
-=======
 import TermsPage from "../views/TermsPage.vue";
 import ContactPage from "../views/ContactView.vue";
->>>>>>> 3b64382ca0cb25f44ea26cc5693a0788a7243366
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
