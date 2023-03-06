8a2180cfb6241614b30d88d264830558ffca385f
Maximon1997 <max@mellanslaget.se>
2023-03-06 13:45:41 +0100

    remove css i aboutview

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -7,13 +7,6 @@ import ChatCards from "../components/ChatCards.vue";
   <ChatCards :start="3"></ChatCards>
 </template>
 
-<style>
-body {
-  background-image: none;
-  background-color: #ffe1e8;
-}
-</style>
-
 <style scoped>
 @media (min-width: 1024px) {
   .about {
