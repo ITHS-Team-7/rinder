979806b24089257baa077f94ae99fce712358384
Ira86 <kudlai.irina@outlook.com>
2023-03-14 14:34:32 +0100

    Added graph to aboutview

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,14 +1,6 @@
 <script setup>
 import AboutCards from "../components/AboutCards.vue";
-<<<<<<< HEAD
-import ChatCards from "../components/ChatCards.vue";
 import UserGraph from "../components/UserGraph.vue";
-</script>
-<template>
-  <AboutCards></AboutCards>
-  <ChatCards :start="3"></ChatCards>
-  <UserGraph></UserGraph>
-=======
 </script>
 <script>
 export default {
@@ -21,7 +13,7 @@ export default {
 </script>
 <template>
   <AboutCards></AboutCards>
->>>>>>> 2fa2efde6a432342e238dddc76c7be7f20a934f9
+  <UserGraph></UserGraph>
 </template>
 
 <style scoped></style>
