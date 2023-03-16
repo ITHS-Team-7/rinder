6cf746fde08340a73d54916ee0102488540306d1
Maximon1997 <max@mellanslaget.se>
2023-03-16 14:06:11 +0100

    Fixat darkmode bug på contact

--- src/components/ContactPage.vue
+++ src/components/ContactPage.vue
@@ -17,6 +17,36 @@ export default {
     onClick() {
       this.submitted = true;
     },
+    checkDarkMode() {
+      if (this.darkMode === true) {
+        this.turnOnDarkMode();
+      } else {
+        this.turnOffDarkMode();
+      }
+    },
+    turnOnDarkMode() {
+      const body = document.querySelector("body");
+      const text = document.querySelector(".infoClass");
+      body.style.backgroundColor = "#000000";
+      text.style.backgroundColor = "#000000";
+      text.style.color = "#ffe1e8";
+    },
+    turnOffDarkMode() {
+      const body = document.querySelector("body");
+      const text = document.querySelector(".infoClass");
+      body.style.backgroundColor = "#ffe1e8";
+      text.style.backgroundColor = "#ffe1e8";
+      text.style.color = "#252525";
+    },
+  },
+  watch: {
+    darkMode() {
+      if (this.darkMode === true) {
+        this.turnOnDarkMode();
+      } else {
+        this.turnOffDarkMode();
+      }
+    },
   },
   computed: {
     btnDisabled() {
@@ -24,8 +54,20 @@ export default {
         this.name.length > 0 && this.email.length > 0 && this.message.length > 0
       );
     },
+    darkMode() {
+      return this.$store.state.Darkmode;
+    },
+  },
+  mounted() {
+    const darkMode = localStorage.getItem("Darkmode");
+    if (darkMode === "true") {
+      this.turnOnDarkMode();
+    } else {
+      this.turnOffDarkMode();
+    }
   },
 };
+
 </script>
 
 <template>
