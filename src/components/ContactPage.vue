fb46871eba14357cf2b82a2b96cd2f7bdefb5022
katzlerjulia <julia.katzler@gmail.com>
2023-03-16 13:51:02 +0100

    tror darkMode problemet löste sig

--- src/components/ContactPage.vue
+++ src/components/ContactPage.vue
@@ -24,14 +24,12 @@ export default {
       const body = document.querySelector("body");
       const text = document.querySelector(".infoClass");
       body.style.backgroundColor = "#000000";
-      text.style.backgroundColor = "#000000";
       text.style.color = "#ffe1e8";
     },
     turnOffDarkMode() {
       const body = document.querySelector("body");
       const text = document.querySelector(".infoClass");
       body.style.backgroundColor = "#ffe1e8";
-      text.style.backgroundColor = "#ffe1e8";
       text.style.color = "#252525";
     },
     mounted() {
